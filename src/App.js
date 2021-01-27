14a4d792746386ebccbed04f22ebfaba94b4d8b3
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-01-27 11:24:32 +0000

    experience crud added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Footer.css";
@@ -12,46 +12,14 @@ import OtherProfile from "./components/OtherProfile";
 import FeedPage from "./components/FeedPage";
 
 class App extends React.Component {
-  state = {
-    user: {},
-  };
-
-  componentDidMount = () => {
-    this.fetchProfile();
-  };
-
-  fetchProfile = async () => {
-    try {
-      let response = await fetch(
-        "https://striveschool-api.herokuapp.com/api/profile/me",
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.REACT_APP_BE_URL}`,
-          },
-        }
-      );
-      let parsedResponse = await response.json();
-
-      this.setState({ user: parsedResponse });
-    } catch (error) {
-      console.log(error);
-    }
-  };
   render() {
     return (
       <BrowserRouter>
         <NavbarApp />
         <Route path="/" exact component={ProfilePage} />
         <Route path="/profile/:id" component={OtherProfile} />
-        {this.state.user && (
-          <Route
-            path="/feed"
-            exact
-            render={(props) => <FeedPage {...props} user={this.state.user} />}
-          />
-        )}
+        <Route path="/feed" exact render={(props) => <FeedPage {...props} />} />
         <Route path="/experience" exact component={Experience} />
-
         <Footer />
       </BrowserRouter>
     );
