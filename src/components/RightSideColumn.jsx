b303ce4c33b04af29650c478bc9c77036afce8d4
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-01-27 12:54:09 +0000

    connected other profiles to frontend

--- src/components/RightSideColumn.jsx
+++ src/components/RightSideColumn.jsx
@@ -3,14 +3,39 @@ import LinkedinLearning from "./LinkedinLearning";
 import PeopleRelated from "./PeopleRelated";
 
 class RightSideColumn extends React.Component {
+  state = {
+    other: [],
+  };
+  componentDidMount = () => {
+    this.fetchProfiles();
+  };
+  fetchProfiles = async () => {
+    try {
+      let response = await fetch(process.env.REACT_APP_SERVER + "/profile/");
+      let parsedResponse = await response.json();
+      const filteredProfiles = parsedResponse.filter(
+        (profile) => profile._id !== process.env.REACT_APP_ME
+      );
+      console.log(filteredProfiles);
+      this.setState({ other: filteredProfiles });
+    } catch (error) {
+      console.log(error);
+    }
+  };
   render() {
     return (
       <div
         style={{ minWidth: "312px", position: "relative", top: "0px" }}
         className="col-12 col-lg-4"
       >
-        <PeopleRelated title="People also viewed" arrPosition={25} />
-        <PeopleRelated title="People you may know" arrPosition={5} />
+        <PeopleRelated
+          title="People also viewed"
+          others={this.state.other.slice(0, 5)}
+        />
+        <PeopleRelated
+          title="People you may know"
+          others={this.state.other.slice(5, 5)}
+        />
         <LinkedinLearning />
       </div>
     );
